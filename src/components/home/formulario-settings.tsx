50a9d1bae85c32a8ed34577de6bf255048ac9022
deste04 <destefano.alessandro2004@gmail.com>
2026-05-13 12:03:23 +0200

    🩹 [FIX] nessuna descrizione in settings

--- src/components/home/formulario-settings.tsx
+++ src/components/home/formulario-settings.tsx
@@ -240,7 +240,11 @@ export function FormularioSettings({ formularioId }: Readonly<{ formularioId: st
                                             <div className="flex flex-col gap-6">
                                                 <div>
                                                     <h3 className="text-sm font-medium mb-1">Descrizione</h3>
-                                                    <p className="text-sm text-muted-foreground">{formulario.descrizione}</p>
+                                                    {formulario.descrizione == '' ? (
+                                                        <p className="text-sm text-muted-foreground">Nessuna descrizione</p>
+                                                    ) : (
+                                                        <p className="line-clamp-3">{formulario.descrizione}</p>
+                                                    )}
                                                 </div>
                                             </div>
                                         )}
