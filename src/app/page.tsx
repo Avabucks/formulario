9d0ba612c284ec0039593c87f84733c950d59ff3
deste04 <destefano.alessandro2004@gmail.com>
2026-07-07 15:15:23 +0200

    🔀 [MERGE] remote main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,35 +43,7 @@ export default function Home() {
   );
 }
 
-<<<<<<< HEAD
-// TODOs:
-
-// - [BUG]
-// - mettere webhook paddle
-// - creare le tabelle nel db per i pagamenti e testare tutto il flow di acquisto, rinnovo, cancellazione, ecc.
-
-// - [EDITOR]
-// - refactor formatting-utils
-
-// - [GENERALI]
-// - report che manda mail a admin (mail nell'env) solo se la formula non è tua (in impostazioni)
-// - aggiornare video e formulario di benvenuto
-
-// - [PREMIUM]
-// - abbonamento che ti da accesso illimitato a creazione formulari e tokens cosi e piu preciso ma senza scrivere il ilmite, semplicemente quando finiscono esce popup
-// - creazione del formulario vuoto/da files con AI (es. carica un pdf e te lo trasforma in formulario)
-// - image manager nelle impostazioni
-
-// - [NOVITA]
-// - ⁠categorie per organizzare formulari + colori titoli
-// - aggiungere funzionalità di esportazione (pdf, markdown, ecc.)
-// - link share solo con chi selezioni
-// - supporto piu lingue
-
-// - [LUNGO TERMINE]
-=======
 // - [IDEE]
->>>>>>> 19ab209e1d1e04e50f47d16839945dfeb3b8c189
 // - flashcards con ai per ogni argomento
 // - console errori della preview
 // - note protette da password
